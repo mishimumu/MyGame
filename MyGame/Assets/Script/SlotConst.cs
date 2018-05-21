e61d42ba6ab538fe1641593ec62df9411a5854ce
mishimumu <793978044@qq.com>
2018-05-21 17:16:56 +0800

    Create SlotConst.cs

---
+++ MyGame/Assets/Script/SlotConst.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotConst  {
+
+    public const string Slot_RefreshUI = "Slot_RefreshUI";
+    public const string Slot_Start = "Slot_Start";
+    public const string Slot_Init = "Slot_Init";
+
+}
