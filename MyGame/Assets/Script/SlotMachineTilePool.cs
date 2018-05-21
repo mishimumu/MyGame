f06fb8921738934965798addaec1a6a73bd78bc1
mishimumu <793978044@qq.com>
2018-05-21 17:15:41 +0800

    Create SlotMachineTilePool.cs

---
+++ MyGame/Assets/Script/SlotMachineTilePool.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotMachineTilePool : MonoBehaviour
+{
+
+    private byte[] tiles;
+
+    private byte[] rewardTiles;
+
+    private List<byte> enableTiles;
+
+    public byte[] RewardTiles { get; private set; }
+
+    public SlotMachineTilePool(byte[] tiles)
+    {
+        this.tiles = tiles;
+        enableTiles = new List<byte>(tiles);
+    }
+
+    public void Reward(byte[] tiles)
+    {
+        RewardTiles = tiles;
+    }
+
+    public void RemoveReward()
+    {
+        //remove reward tile
+        foreach (var tile in RewardTiles)
+        {
+            if (enableTiles.Contains(tile))
+            {
+                enableTiles.Remove(tile);
+            }
+        }
+    }
+
+    public void Reset()
+    {
+        enableTiles = new List<byte>(tiles);
+    }
+
+    public byte GetTiles(byte tile)
+    {
+        int random = Random.Range(0, enableTiles.Count);
+        byte newTile = enableTiles[random];
+        enableTiles.Remove(newTile);
+        enableTiles.Add(tile);
+        return newTile;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
