5c6eab1211081bb1c8199cf7c10e8f5968c8d8dc
mishimumu <793978044@qq.com>
2018-05-21 20:22:28 +0800

    Update SlotCell.cs

--- MyGame/Assets/Script/SlotCell.cs
+++ MyGame/Assets/Script/SlotCell.cs
@@ -2,21 +2,37 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
-public class SlotCell : BaseView
+public class SlotCell : MonoBehaviour
 {
+    private byte index;
+    public byte Index
+    {
+        get
+        {
+            return index;
+        }
 
-    public bool isToggle;
-    public byte slotType;
-    public byte index;
-    public delegate void scrollDelegate();
-    public SlotController controller;
-    public float limitHeight;
-    public bool isReturn;
-    public float speed;
-    public GameObject image;
+        set
+        {
+            index = value;
+        }
+    }
+
+    private float limitHeight;
+    private bool isReturn;
+    public bool IsReturn
+    {
+        get
+        {
+            return isReturn;
+        }
+
+        private set
+        {
+            isReturn = value;
+        }
+    }
     private byte tile;
-    Vector3 pos;
-
     public byte Tile
     {
         get
@@ -34,100 +50,13 @@ public class SlotCell : BaseView
     public void Init(float limitHeight)
     {
         this.limitHeight = limitHeight;
-        pos = transform.position;
-    }
-
-    protected override void OnUpDate()
-    {
-        // ScrollMove();
-        if (pos.y != transform.position.y)
-        {
-            speed = Mathf.Abs(transform.position.y - pos.y);
-        }
-        else
-        {
-            speed = 0;
-        }
-        pos = transform.position;
+        gameObject.SetActive(true);
     }
 
     public void Move(float speed)
     {
         GetComponent<RectTransform>().anchoredPosition3D -= new Vector3(0f, speed, 0f);
-        isReturn = GetComponent<RectTransform>().anchoredPosition3D.y <= limitHeight;
-    }
-
-    public bool IsOverLimit()
-    {
-        return GetComponent<RectTransform>().anchoredPosition3D.y <= limitHeight;
-    }
-
-    public void DecreaseMove(float speed)
-    {
-        GetComponent<RectTransform>().anchoredPosition3D -= new Vector3(0f, speed, 0f);
-    }
-
-    public void Return(float time, Vector3 target, iTween.EaseType easetype)
-    {
-        iTween.MoveTo(gameObject, iTween.Hash("time", time, "position", target, "easetype", easetype, "oncomplete", "FinishAnimation", "oncompletetarget", gameObject, "islocal", false));
-    }
-
-
-
-    void ScrollMove()
-    {
-        if (!controller.isStop)
-        {
-
-            if (transform.localPosition.y < controller.limitHeight)
-            {
-                float distance = transform.localPosition.y - controller.height;
-                GetComponent<RectTransform>().anchoredPosition3D += new Vector3(0f, controller.height, 0f);
-            }
-            else
-            {
-                if (isToggle && controller.isShowResult)
-                {
-
-                    float pos = 0f;
-                    if (transform.localPosition.y > pos)
-                    {
-                        GetComponent<RectTransform>().anchoredPosition3D += new Vector3(0f, -controller.speed, 0f);
-                    }
-                    else
-                    {
-                        float value = transform.localPosition.y + controller.speed;
-                        if (value >= pos)
-                        {
-                            GetComponent<RectTransform>().anchoredPosition3D = new Vector3(GetComponent<RectTransform>().anchoredPosition3D.x, 0f, 0);
-                            if (controller.stopAction != null)
-                            {
-                                controller.stopAction(controller);
-                            }
-
-                        }
-                        else
-                        {
-                            GetComponent<RectTransform>().anchoredPosition3D += new Vector3(0f, -controller.speed, 0f);
-                        }
-                    }
-                }
-                else
-                {
-                    GetComponent<RectTransform>().anchoredPosition3D += new Vector3(0f, -controller.speed, 0f);
-                }
-            }
-
-        }
-
-
-
-    }
-
-    public void SetIsToggle(bool isToggle)
-    {
-        this.isToggle = isToggle;
-
+        IsReturn = GetComponent<RectTransform>().anchoredPosition3D.y <= limitHeight;
     }
 
 }
